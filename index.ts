6e32c6c66f5a2cb86bc98fa6b763c350e3ebe072
Gabe <gabe10777@gmail.com>
2023-07-04 18:59:13 -0700

    add gen dependency report, more logging

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import 'module-alias/register';
 import * as dotenv from 'dotenv';
 import scoMom from '@/discord';
 
+import { generateDependencyReport } from '@discordjs/voice';
+
 export function config() {
     dotenv.config({
         path: process.env.NODE_DEV === 'true' ? '.dev.env' : '.env',
@@ -10,4 +12,5 @@ export function config() {
 
 config();
 
+console.log(generateDependencyReport());
 scoMom().then(() => console.log('scomom initialized'));
